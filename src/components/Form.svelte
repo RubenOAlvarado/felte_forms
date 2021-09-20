bbee8b5649d87d61b555a1d9b91d1ac842010af4
RubenOAlvarado <rubenosmaralvarado@gmail.com>
2021-09-19 19:24:33 -0500

    css framework integration and components initialization

---
+++ src/components/Form.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { createForm } from "felte";
+    import { validator } from "@felte/validator-yup";
+    import * as yup from "yup";
+    import reporterDom from "@felte/reporter-dom";
+
+    const { form } = createForm({
+        extend: [validator, reporterDom()],
+    });
+</script>
+
+<div class="nes-container">
+    <form use:form />
+</div>
